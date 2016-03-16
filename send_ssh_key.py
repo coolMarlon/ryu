b6e99a225d6c7fc3b9f4b68ab76c025ff934e475
paradisecr <704645806@qq.com>
2016-03-16 12:04:10 +0800

    fix send_ssh_key error

--- send_ssh_key.py
+++ send_ssh_key.py
@@ -2,15 +2,16 @@ import os
 import OXP_CFG
 
 # generate ssh key.
-os.system("ssh-keygen -t rsa")
+os.system('''if [ ! -f ~/.ssh/id_rsa ]  || [ ! -f ~/.ssh/id_rsa.pub ];\
+        then ssh-keygen -t rsa; \
+        fi''')
 
 USER = OXP_CFG.USER
 DOMAINS = OXP_CFG.DOMAINS
 
 for domain in DOMAINS:
     os.system("ssh %s@%s mkdir '.ssh; chmod 700 .ssh'" % (USER, domain))
-    os.system("scp ~/.ssh/id_rsa.pub %s@%s:~/.ssh/id_rsa.pub" % (USER, domain))
     os.system(
         "ssh %s@%s 'touch ~/.ssh/authorized_keys;\
-        chmod 600 ~/.ssh/authorized_keys;\
-        cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys'" % (USER, domain))
+        chmod 600 ~/.ssh/authorized_keys'" % (USER, domain))
+    os.system("ssh-copy-id %s@%s" % (USER, domain))
